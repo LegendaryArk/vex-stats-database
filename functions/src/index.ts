2f6240efd116bf218f5b37a97666c3a429ab43bc
Noah <noahsun0418@gmail.com>
2025-06-25 23:15:04 -0400

    Firebase Setup and Testing

---
+++ functions/src/index.ts
@@ -0,0 +1,54 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import { setGlobalOptions } from "firebase-functions";
+import { onRequest } from "firebase-functions/https";
+import * as logger from "firebase-functions/logger";
+
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import { onDocumentCreated } from "firebase-functions/firestore";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// For cost control, you can set the maximum number of containers that can be
+// running at the same time. This helps mitigate the impact of unexpected
+// traffic spikes by instead downgrading performance. This limit is a
+// per-function limit. You can override the limit for each function using the
+// `maxInstances` option in the function's options, e.g.
+// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
+// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
+// functions should each use functions.runWith({ maxInstances: 10 }) instead.
+// In the v1 API, each function can only serve one request per container, so
+// this will be the maximum concurrent request count.
+setGlobalOptions({ maxInstances: 10, timeoutSeconds: 300 });
+
+export const helloWorld = onRequest((request, response) => {
+	logger.info("Hello logs!", { structuredData: true });
+	response.send("Hello from Firebase!");
+});
+
+export const addMessage = onRequest(async (req, res) => {
+	logger.info("messages");
+	const original = req.query.text;
+	const writeRes = await getFirestore().collection("messages").add({ original: original });
+	res.json({ result: `Message with ID: ${writeRes.id} added` });
+});
+
+export const makeUppercase = onDocumentCreated("messages/{documentId}", (event) => {
+	const original = event.data?.data().original;
+	logger.log("Uppercasing", event.params.documentId, original);
+
+	const uppercase = original.toUpperCase();
+
+	return event.data?.ref.set({ uppercase }, { merge: true });
+});
+
+initializeApp();
